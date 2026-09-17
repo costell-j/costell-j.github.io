e6890aad5cec714cf8addb9d7febaa8445667f2a
Costell Johnson <davaunjohnson0@gmail.com>
2026-09-17 11:45:56 -0400

    Following along in class

--- csce242/exercises/04js/scripts/intro.js
+++ csce242/exercises/04js/scripts/intro.js
@@ -16,3 +16,70 @@ document.getElementById("btn-bounce").onclick = (e) => {
     console.log("salutations");
     document.getElementById("ball").classList.toggle("bouncing-ball");
 }
+
+document.getElementById("txt-num-days").onkeyup = (e) => {
+    const numDays = parseInt(e.target.value);
+    const pMessage = document.getElementById("p-plant-msg");
+    const plantImage = document.getElementById("plant-img");
+    plantImage.classList.remove("hidden");
+    if(numDays <= 2) {
+        pMessage.innerHTML = `Let your plant rest, its only been ${numDays} days.`;
+        plantImage.src="https://dummyimage.com/150x150/07d607/fff&text=Happy+plant";
+    }
+    else if(numDays <= 5) {
+        pMessage.innerHTML = `Time to water, its been ${numDays} days`;
+    }
+    else if( numDays <= 7) {
+        pMessage.innerHTML = `Oh no! Your plant is wilting, it's been ${numDays} days`;
+    } else {
+        pMessage.innerHTML = "RIP my plant they got hit by a bazooka";
+    }
+}
+
+
+//counting 
+let countInterval;
+let count = 0;
+const pCount = document.getElementById("p-count");
+const btnStart = document.getElementById("btn-start");
+const btnPause = document.getElementById("btn-pause");
+const btnStop = document.getElementById("btn-stop");
+btnPause.disabled = true;
+btnStop.disabled = true;
+
+
+btnStart.onclick = () => {
+    countInterval = setInterval(() => {
+        pCount.innerHTML = ++count;
+    },500);
+    btnStart.disabled = true;
+    btnPause.disabled = false;
+    btnStop.disabled = false;
+}
+
+btnPause.onclick = () => {
+    console.log("Pause clicked");
+    clearInterval(countInterval);
+    btnStart.disabled = false;
+    btnPause.disabled = true;
+    btnStop.disabled = false;
+}
+
+btnStop.onclick = () => {
+    console.log("Stop clicked");
+    count = 0;
+    pCount.innerHTML = "";
+    clearInterval(countInterval);
+    btnStart.disabled = false;
+    btnPause.disabled = true;
+    btnStop.disabled = true;
+}
+
+//date display
+setInterval(() => {
+    const pDisplay = document.getElementById("date-display");
+    const today = new Date();
+    const seconds = today.getSeconds();
+    pDisplay.innerHTML = seconds;
+
+},1000);
