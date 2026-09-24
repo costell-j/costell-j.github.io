43a30ef17cbdaf9f80c7a6c20384b2d9e825e30d
Costell Johnson <davaunjohnson0@gmail.com>
2026-09-24 14:16:59 -0400

    followed along in class and finished css art

--- csce242/exercises/04js/scripts/loops.js
+++ csce242/exercises/04js/scripts/loops.js
@@ -47,9 +47,62 @@ document.getElementById("btn-show-toys").onclick = (e) => {
     const toyList = document.getElementById("toy-list");
     toyList.innerHTML = "";
 
-    for(let i = 0; i < toys.length; i++) {
+    /*for(let i = 0; i < toys.length; i++) {
         const p = document.createElement("p");
         p.innerHTML = toys(i);
         toyList.append(p);
-    }
+    }*/
+
+    toys.forEach((toy) => {
+        const p = document.createElement("p").innerHTML;
+        p.innerHTML = toy;
+        toyList.append(p);
+    })
 }
+
+//show a table of toy prices
+document.getElementById("btn-show-toys-prices").onclick = (e) => {
+    const div = document.getElementById("toy-info");
+    div.innerHTML = "";
+
+    const toyMap = [];
+    toyMap["doll"] = 129.99;
+    toyMap["skateboard"] = 200.00;
+    toyMap["mini car"] = 20.00;
+    toyMap["board game"] = 15.00;
+    toyMap["nuclear weapon"] = 0.52; //on the black market
+
+    const table = document.createElement("table");
+    div.append(table);
+    let tr = document.createElement("tr");
+    table.append(tr);
+    let th = document.createElement("th");
+    tr.append(th);
+    th.innerHTML = "Name";
+    th = document.createElement("th");
+    tr.append(th);
+    th.innerHTML = "Price";
+
+    for(let toy in toyMap) {
+        table.append(createTR(toy, "$" + toyMap[toy]));
+    }
+};
+
+const createTD = (data) => {
+    const td = document.createElement("td");
+    td.innerHTML = data;
+    return td;
+};
+
+const createElement = (data, type) => {
+    const elem = document.createElement(type);
+    elem.innerHTML = data;
+    return elem;
+}
+
+const createTR = (data1, data2) => {
+    const tr = document.createElement("tr");
+    tr.append(createElement(data1, "td"));
+    tr.append(createElement(data2, "td"));
+    return tr;
+};
---
+++ csce242/exercises/04js/scripts/slideshow.js
@@ -0,0 +1,4 @@
+document.getElementById("hero-arrow-right").onclick = (e) => {
+    e.preventDefault();
+    const currentSlide = document.querySelector("#slides not(.hidden)");
+}
